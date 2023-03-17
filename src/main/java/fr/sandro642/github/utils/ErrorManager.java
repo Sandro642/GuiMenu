cc7c8c2164d41f36e4e6272208212cf1c4009632
Sandro642 <sandro33810@gmail.com>
2023-03-17 18:33:00 +0100

    Update of plugin

--- src/main/java/fr/sandro642/github/utils/ErrorManager.java
+++ src/main/java/fr/sandro642/github/utils/ErrorManager.java
@@ -1,4 +1,12 @@
 package fr.sandro642.github.utils;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
 public class ErrorManager {
+
+    public static String ErrorLobbys(Player p) {
+        Bukkit.getConsoleSender().sendMessage("§cVous ne pouvez pas vous connecter à un lobby car il sont en maintenance!");
+        return null;
+    }
 }
